4ecab13ce85c93c0c03a586d099c22780bf34fb6
Jonathan Hanna <jonathanhanna@Jonathans-MBP.home>
2020-05-05 18:45:13 -0400

    test dev setup

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
 	name='tensorplane',
-	version='0.1.0',
+	version='0.0.1',
 	author='Jonathan Hanna',
 	packages=['tensorplane'],
 	license='LICENSE.txt',
+	url='git@github.com:JonathanHanna/tensorplane.git',
 	description='A modular and expressive interface for managing data in Python',
 	long_description=open('README.md').read(),
 	install_requires=[
