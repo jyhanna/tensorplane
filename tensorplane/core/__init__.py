52483b7aacac39091a4dcd43a58ed90d459c47f1
Jonathan Hanna <jonathanhanna@Jonathans-MBP.home>
2020-05-05 22:38:53 -0400

    filled out init.py

--- tensorplane/core/__init__.py
+++ tensorplane/core/__init__.py
@@ -1,1 +1,5 @@
+__all__ = [
+    'data',
+]
+
 from . import data
---
+++ tensorplane/core/lib/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'backend',
+    'attrs',
+    'index',
+    'utils',
+]
+
+from . import backend
+from . import attrs
+from . import index
+from . import utils
