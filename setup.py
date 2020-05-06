3220d94cb7d363549a2a306ce716fdb12287b676
Jonathan Hanna <jonathanhanna@Jonathans-MBP.home>
2020-05-05 22:26:55 -0400

    reorganize module structure

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name='tensorplane',
 	version='0.0.1',
 	author='Jonathan Hanna',
-	packages=['tensorplane'],
+	packages=find_packages(),
 	license='LICENSE.txt',
 	url='git@github.com:JonathanHanna/tensorplane.git',
 	description='A modular and expressive interface for managing data in Python',
